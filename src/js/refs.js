9d241a842cb22e46da844b2c0081213b59cb9178
yurii2007 <yurash862@gmail.com>
2024-10-16 18:44:15 +0300

    init empty project

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-    searchForm: document.querySelector('#search-form'),
-    galleryElem: document.querySelector('.gallery'),
-    btnLoadMore: document.querySelector('.js-load-more')
-}
